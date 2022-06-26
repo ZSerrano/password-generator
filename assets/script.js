d44edb1084a30cc94d64b743c63b6d23d09cf3ea
Brometheus <zs.serrano1@gmail.com>
2022-06-26 00:47:29 -0500

    added isinteger method in validity statement

--- assets/script.js
+++ assets/script.js
@@ -21,44 +21,46 @@ function generatePassword() {
 
 		let pwdLength = prompt("Enter the number of characters you would like your password to be.", "Password must be between 8-128 characters.");
 		
-
-    if (pwdLength >= 8 && pwdLength <= 128) {
+    if (!Number.isInteger(Number(pwdLength))) {
+      window.alert(pwdLength + " is not a valid input.  Please enter a number between 8-128.");
+      return generatePassword(); 
+    } else if (pwdLength >= 8 && pwdLength <= 128) {
       confirm("Your password will be " + pwdLength + " characters long.");
     } else {
       confirm("Please enter a number between 8-128.");
       return generatePassword();
     };
+    // console.log("Z");
 
     let yesUpper = confirm("Do you want uppercase letters in your password? Hit 'Ok' for yes or 'Cancel' for no.");
-
     let yesLower = confirm("Do you want lowercase letters in your password? Hit 'Ok' for yes or 'Cancel' for no.");
     let yesNumbers = confirm("Do you want to use numbers in your password? Hit 'Ok' for yes or 'Cancel' for no.");
     let yesSpecial = confirm("Do you want special characters in your password? These can include items as such as an exclamation point, question mark, etc. Hit 'Ok' for yes or 'Cancel' for no.");
     
     function validatePass() {
       if (yesUpper === true) {
-        console.log("Yes uppercase");
-        console.log(passChar);
+        // console.log("Yes uppercase");
+        // console.log(passChar);
         passChar = passChar.concat(upper);
       }
   
       if (yesLower === true) {
-        console.log("Yes lowercase");
-        console.log(passChar);
+        // console.log("Yes lowercase");
+        // console.log(passChar);
         passChar = passChar.concat(lower);
       }
       if (yesNumbers === true) {
-        console.log("yes Numbers");
+        // console.log("yes Numbers");
         passChar = passChar.concat(numbers);
       }
       if (yesSpecial === true) {
-        console.log("yes special");
+        // console.log("yes special");
         passChar = passChar.concat(special);
       }
-      console.log(passChar);
+      // console.log(passChar);
       return passChar;
     };
-    // 
+    // if any of the confirms are true, how
     
     validatePass();
 
